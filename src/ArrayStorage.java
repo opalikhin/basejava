23dd7ac32d0e6961b292cb0d3c3d4d4945b5a6f2
opalikhin <o.opalikhin@cft.ru>
2020-05-13 23:17:14 +0500

    HW1. Реализация класса ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,64 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume result = new Resume();
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null && storage[i].uuid == uuid) {
+                result = storage[i];
+                break;
+            }
+        }
+        return result;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null && storage[i].uuid == uuid) {
+                storage[i] = null;
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int storageSize = this.size();
+        int j = 0;
+        Resume[] result = new Resume[storageSize];
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                result[j] = new Resume();
+                result[j] = storage[i];
+                j++;
+            }
+        }
+        return result;
     }
 
     int size() {
-        return 0;
+        int result = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                result++;
+            }
+        }
+        return result;
     }
 }

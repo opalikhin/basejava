229a4767e375f29eac0e81ced44ebc5ea869f069
opalikhin <o.opalikhin@cft.ru>
2020-05-28 12:36:33 +0500

    HW2. Исправление замечаний - круг 1

--- src/com/opalikhin/webapp/storage/ArrayStorage.java
+++ src/com/opalikhin/webapp/storage/ArrayStorage.java
@@ -2,6 +2,8 @@ package com.opalikhin.webapp.storage;
 
 import com.opalikhin.webapp.model.Resume;
 
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -11,9 +13,7 @@ public class ArrayStorage {
     private int size = 0;
 
     public void clear() {
-        for (int i = 0; i < size; i++) {
-            storage[i] = null;
-        }
+        Arrays.fill(storage, 0, size - 1, null);
         size = 0;
     }
 
@@ -52,11 +52,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-        Resume[] result = new Resume[size];
-        for (int i = 0; i < size; i++) {
-            result[i] = storage[i];
-        }
-        return result;
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     public int size() {
